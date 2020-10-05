be9920b1b457cb3009536972083dd1542d3d4e4f
Bangyi Wu <bangyi98@gmail.com>
2020-10-06 03:58:31 +0800

    Fix model stub

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -139,6 +139,11 @@ public class AddCommandTest {
         }
 
         @Override
+        public void sortPerson(Index) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
         public ObservableList<Person> getFilteredPersonList() {
             throw new AssertionError("This method should not be called.");
         }
