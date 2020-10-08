4b4e41c49b83043df5c664160352a5ffd898dbdc
hendeyfan@u.nus.edu <hendeyfan@u.nus.edu>
2020-10-08 18:05:48 +0800

    Removed extra import statements

--- src/main/java/seedu/address/model/event/UniqueEventList.java
+++ src/main/java/seedu/address/model/event/UniqueEventList.java
@@ -10,9 +10,6 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import seedu.address.model.event.exceptions.DuplicateEventException;
 import seedu.address.model.event.exceptions.EventNotFoundException;
-import seedu.address.model.person.Person;
-import seedu.address.model.person.exceptions.DuplicatePersonException;
-import seedu.address.model.person.exceptions.PersonNotFoundException;
 
 /**
  * A list of events that enforces uniqueness between its elements and does not allow nulls.
