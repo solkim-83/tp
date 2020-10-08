48b13e009c0b7237290b6ee764555487a0ae4cf0
hendeyfan@u.nus.edu <hendeyfan@u.nus.edu>
2020-10-08 18:08:22 +0800

    Edited DuplicateEventExceptions to pass checkstyle

--- src/main/java/seedu/address/model/event/exceptions/DuplicateEventException.java
+++ src/main/java/seedu/address/model/event/exceptions/DuplicateEventException.java
@@ -1,11 +1,11 @@
 package seedu.address.model.event.exceptions;
 
 /**
- * Signals that the operation will result in duplicate events (Events are considered duplicates if they have the same
+ * Signals that the operation will result in duplicate Events (Events are considered duplicates if they have the same
  * identity).
  */
 public class DuplicateEventException extends RuntimeException {
     public DuplicateEventException() {
-        super("Operation would result in duplicate Events");
+        super("Operation would result in duplicate events");
     }
 }
