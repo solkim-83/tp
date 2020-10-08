dda2c0ca0d72e4da3ab6be7fc8b5468dff85aa20
hendeyfan@u.nus.edu <hendeyfan@u.nus.edu>
2020-10-08 18:10:19 +0800

    Fix checkstyle attempt 1

--- src/main/java/seedu/address/model/event/exceptions/DuplicateEventException.java
+++ src/main/java/seedu/address/model/event/exceptions/DuplicateEventException.java
@@ -5,7 +5,10 @@ package seedu.address.model.event.exceptions;
  * identity).
  */
 public class DuplicateEventException extends RuntimeException {
+
     public DuplicateEventException() {
         super("Operation would result in duplicate events");
     }
+
 }
+
