8c8e00a098153bae2a9934d5848594298b0e47b9
hendeyfan@u.nus.edu <hendeyfan@u.nus.edu>
2020-10-21 03:31:27 +0800

    Add calendar to StorageManager and its relevant interface changes

--- src/main/java/seedu/address/storage/StorageManager.java
+++ src/main/java/seedu/address/storage/StorageManager.java
@@ -8,6 +8,7 @@ import java.util.logging.Logger;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.exceptions.DataConversionException;
 import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.ReadOnlyCalendar;
 import seedu.address.model.ReadOnlyUserPrefs;
 import seedu.address.model.UserPrefs;
 
@@ -18,14 +19,19 @@ public class StorageManager implements Storage {
 
     private static final Logger logger = LogsCenter.getLogger(StorageManager.class);
     private AddressBookStorage addressBookStorage;
+    private CalendarStorage calendarStorage;
     private UserPrefsStorage userPrefsStorage;
 
     /**
      * Creates a {@code StorageManager} with the given {@code AddressBookStorage} and {@code UserPrefStorage}.
      */
-    public StorageManager(AddressBookStorage addressBookStorage, UserPrefsStorage userPrefsStorage) {
+    public StorageManager(AddressBookStorage addressBookStorage,
+                          CalendarStorage calendarStorage,
+                          UserPrefsStorage userPrefsStorage)
+    {
         super();
         this.addressBookStorage = addressBookStorage;
+        this.calendarStorage = calendarStorage;
         this.userPrefsStorage = userPrefsStorage;
     }
 
@@ -74,6 +80,35 @@ public class StorageManager implements Storage {
     public void saveAddressBook(ReadOnlyAddressBook addressBook, Path filePath) throws IOException {
         logger.fine("Attempting to write to data file: " + filePath);
         addressBookStorage.saveAddressBook(addressBook, filePath);
+    }
+
+    // ================ Calendar methods ==============================
+
+    @Override
+    public Path getCalendarFilePath() {
+        return addressBookStorage.getAddressBookFilePath();
+    }
+
+    @Override
+    public Optional<ReadOnlyCalendar> readCalendar() throws DataConversionException, IOException {
+        return readCalendar(calendarStorage.getCalendarFilePath());
+    }
+
+    @Override
+    public Optional<ReadOnlyCalendar> readCalendar(Path filePath) throws DataConversionException, IOException {
+        logger.fine("Attempting to read data from file: " + filePath);
+        return calendarStorage.readCalendar(filePath);
+    }
+
+    @Override
+    public void saveCalendar(ReadOnlyCalendar calendar) throws IOException {
+        saveCalendar(calendar, calendarStorage.getCalendarFilePath());
+    }
+
+    @Override
+    public void saveCalendar(ReadOnlyCalendar calendar, Path filePath) throws IOException {
+        logger.fine("Attempting to write to data file: " + filePath);
+        calendarStorage.saveCalendar(calendar, filePath);
     }
 
 }
