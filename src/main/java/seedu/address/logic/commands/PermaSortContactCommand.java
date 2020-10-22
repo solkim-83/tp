7066afc3622f10164a1f8bb36b73407b72722012
Bangyi Wu <bangyi98@gmail.com>
2020-10-22 21:57:55 +0800

    Fix checkstyle v2

--- src/main/java/seedu/address/logic/commands/PermaSortContactCommand.java
+++ src/main/java/seedu/address/logic/commands/PermaSortContactCommand.java
@@ -1,9 +1,8 @@
 package seedu.address.logic.commands;
 
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
 import java.util.Comparator;
-
-
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
