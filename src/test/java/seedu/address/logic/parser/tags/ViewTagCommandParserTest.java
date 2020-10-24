6ff7b4adb679c56eedbaba642d3ffaf450721a6c
Illio Suardi <illio.suardi@gmail.com>
2020-10-25 01:31:28 +0800

    Repackage contact- and tag-related parser tests

--- src/test/java/seedu/address/logic/parser/tags/ViewTagCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/tags/ViewTagCommandParserTest.java
@@ -1,4 +1,4 @@
-package seedu.address.logic.parser;
+package seedu.address.logic.parser.tags;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
