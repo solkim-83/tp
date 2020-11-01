e92f39d5ea85c91aae70ae0aad7031433686defa
hendeyfan@u.nus.edu <hendeyfan@u.nus.edu>
2020-11-01 23:28:43 +0800

    Add more tests to TimeTest

--- src/test/java/seedu/address/model/event/TimeTest.java
+++ src/test/java/seedu/address/model/event/TimeTest.java
@@ -37,7 +37,15 @@ public class TimeTest {
 
     @Test
     public void getDisplayNameTest() {
-        Time time = new Time("11-12-1234 12:34");
-        assertEquals(time.getDisplayName().substring(0, 17), "Mon 11th Dec 1234");
+        Time time1 = new Time("11-12-1234 12:34");
+        assertEquals("Mon 11th Dec 1234", time1.getDisplayName().substring(0, 17));
+        Time time2 = new Time("21-12-1234 12:34");
+        assertEquals("Thu 21st Dec 1234", time2.getDisplayName().substring(0, 17));
+        Time time3 = new Time("22-12-1234 12:34");
+        assertEquals("Fri 22nd Dec 1234", time3.getDisplayName().substring(0, 17));
+        Time time4 = new Time("23-12-1234 12:34");
+        assertEquals("Sat 23rd Dec 1234", time4.getDisplayName().substring(0, 17));
+        Time time5 = new Time("31-12-1234 12:34");
+        assertEquals("Sun 31st Dec 1234", time5.getDisplayName().substring(0, 17));
     }
 }
