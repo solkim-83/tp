06ac01d0eeae9d63ea0236d0de33b53d26eccd67
Bangyi Wu <bangyi98@gmail.com>
2020-11-03 00:19:36 +0800

    Fix import order

--- src/test/java/seedu/address/logic/commands/contacts/DeleteContactByTagCommandTest.java
+++ src/test/java/seedu/address/logic/commands/contacts/DeleteContactByTagCommandTest.java
@@ -11,8 +11,8 @@ import seedu.address.model.Calendar;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.person.Person;
 import seedu.address.model.RemindersImpl;
+import seedu.address.model.person.Person;
 import seedu.address.model.tag.Tag;
 import seedu.address.model.tag.TagTreeImpl;
 
