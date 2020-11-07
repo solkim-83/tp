77e2c5f41d37f3e77d4482b992e661c2b5fc87ae
hendeyfan@u.nus.edu <hendeyfan@u.nus.edu>
2020-11-07 19:10:55 +0800

    Fix implementation error

--- src/main/java/seedu/address/logic/commands/contacts/ClearContactCommand.java
+++ src/main/java/seedu/address/logic/commands/contacts/ClearContactCommand.java
@@ -2,12 +2,13 @@ package seedu.address.logic.commands.contacts;
 
 import static java.util.Objects.requireNonNull;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.CommandType;
 import seedu.address.logic.commands.CommandWord;
 import seedu.address.model.Model;
-import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.person.Person;
 
 /**
@@ -26,8 +27,10 @@ public class ClearContactCommand extends Command {
         requireNonNull(model);
         // This implementation is inefficient but it will remove said person from both the contact and event list.
         // model.deletePerson removes the person from any event they attend
-        ReadOnlyAddressBook addressBook = model.getAddressBook();
-        for (Person person : addressBook.getPersonList()) {
+        ObservableList<Person> personObservableList = FXCollections.observableArrayList();
+        // make a copy of the list as the direct list from getPersonList is affected by deletePerson
+        personObservableList.setAll(model.getAddressBook().getPersonList());
+        for (Person person : personObservableList) {
             model.deletePerson(person);
         }
         return new CommandResult(MESSAGE_SUCCESS);
